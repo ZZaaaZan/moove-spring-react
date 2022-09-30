16236931cb5096eb059462a3beb2717f37052c1d
byunyc0124 <baeyonyk124@gmail.com>
2022-09-30 21:27:12 +0900

    init

---
+++ src/main/java/snowz/moove/MooveApplication.java
@@ -0,0 +1,13 @@
+package snowz.moove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MooveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/snowz/moove/MooveApplicationTests.java
@@ -0,0 +1,13 @@
+package snowz.moove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
