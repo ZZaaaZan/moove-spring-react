c6c1efd7655e77d97fa411772a5590a2a06c06a1
byunyc0124 <baeyonyk124@gmail.com>
2022-10-22 00:58:44 +0900

    fix: webConfig 수정

--- src/main/java/snowz/moove/config/WebConfig.java
+++ src/main/java/snowz/moove/config/WebConfig.java
@@ -3,6 +3,8 @@ package snowz.moove.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import snowz.moove.config.auth.LoginUserArgumentResolver;
 
@@ -10,11 +12,19 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
         argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 }
